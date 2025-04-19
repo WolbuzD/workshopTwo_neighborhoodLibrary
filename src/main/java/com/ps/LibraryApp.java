5ab584a988169ed90d6d1366cf57f05e34f56531
Thierno <podiallo2242@gmail.com>
2025-04-19 16:00:18 -0400

    Created an Array with capacity 20 and filled in the array with sample books

--- src/main/java/com/ps/LibraryApp.java
+++ src/main/java/com/ps/LibraryApp.java
@@ -1,7 +1,19 @@
 package com.ps;
 
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
 public class LibraryApp {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+     //Let's create an Array to hold the inventory of 20 books
+        Book[] books = new Book[20];
 
+        //Now we need to fill in sample books using a Loop:
+        for(int i=0; i<20; i++){
+            books[i]= new Book(i + 1, "ISBN" + (1000 + i), "Book Title " + (i + 1));
+
+           // System.out.println(books[i].getId()+" "+ books[i].getIsbn() +" "+ books[i].getTitle());
+        }
     }
 }
