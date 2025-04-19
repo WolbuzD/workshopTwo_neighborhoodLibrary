a39d9b2c478e8c71160de5916153e1b42c21785c
Thierno <podiallo2242@gmail.com>
2025-04-19 16:49:44 -0400

    completed the Show available books(user entered 1)

--- src/main/java/com/ps/LibraryApp.java
+++ src/main/java/com/ps/LibraryApp.java
@@ -11,7 +11,7 @@ public class LibraryApp {
 
         //Now we need to fill in sample books using a Loop:
         for(int i=0; i<20; i++){
-            books[i]= new Book(i + 1, "ISBN" + (1000 + i), "Book Title " + (i + 1));
+            books[i]= new Book(i + 1, "ISBN" + (1000 + i), "Book Title" + (i + 1));
 
            // System.out.println(books[i].getId()+" "+ books[i].getIsbn() +" "+ books[i].getTitle());
         }
@@ -27,6 +27,36 @@ public class LibraryApp {
             System.out.println("3. Exit");
             System.out.print("Enter your choice (1-3): ");
             String choice = scanner.nextLine();
+
+            if(choice.equals("1")){
+                //show available books(meaning the books that are not checked out)
+                for(int i =0; i< books.length; i++){
+                    if(!books[i].isCheckedOut()){
+                        System.out.println("ID: " + books[i].getId()+"ISBN: " + books[i].getIsbn() + ", Title: " + books[i].getTitle());
+                    }
+                }
+
+                // Ask user if they want to check out a book
+                System.out.print("\nEnter the ID of the book you want to check out (0 to go back): ");
+                int bookId = Integer.parseInt(scanner.nextLine());
+
+                if(bookId !=0){
+                    for (int i = 0; i < books.length; i++) {
+                        if (books[i].getId() == bookId && !books[i].isCheckedOut()) {
+                            System.out.print("Enter your name: ");
+                            String name = scanner.nextLine();
+                            books[i].checkOut(name);
+                            System.out.printf("The Book %s with the ID %d is checked out successfully!",books[i].getTitle(), books[i].getId());
+                        }
+                    }
+                }
+
+            }
+
+
         }
+
+
+
     }
 }
