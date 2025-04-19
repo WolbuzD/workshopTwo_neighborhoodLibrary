f45b705e8744f2974117259976dfec6ab110ec2a
Thierno <podiallo2242@gmail.com>
2025-04-19 15:39:21 -0400

    completed the Book Class(properties, getters, setters, other methods)

--- src/main/java/com/ps/Book.java
+++ src/main/java/com/ps/Book.java
@@ -1,4 +1,73 @@
 package com.ps;
 
 public class Book {
+    //properties
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean isCheckedOut;
+    private String checkedOutTo;
+
+    public Book(int id, String isbn, String title) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckedOut = false;
+        this.checkedOutTo = "";
+    }
+   //getters
+    public int getId() {
+        return id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+    //Setters
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
+    //additional methods:
+
+// Check out the book
+    public void checkOut(String name) {
+        isCheckedOut = true;
+        checkedOutTo = name;
+    }
+
+    // Check in the book
+    public void checkIn() {
+        isCheckedOut = false;
+        checkedOutTo = "";
+    }
 }
